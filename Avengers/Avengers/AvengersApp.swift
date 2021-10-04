7ba95d01d2ed1aa709bce8b34932f935bcaaf23c
Yan Akrameev <ianm2024@gmail.com>
2021-10-04 12:58:20 -0300

    initial commit, starting project

---
+++ Avengers/Avengers/AvengersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvengersApp.swift
+//  Avengers
+//
+//  Created by Yan Akhrameev on 04/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AvengersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
